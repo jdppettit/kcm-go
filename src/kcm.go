8544c25d4301cc7dc244836333486ad7989b9e63
Joseph Pettit <jpettit@pettitservers.com>
2021-06-10 22:17:02 -0500

    read/write configs added

--- src/kcm.go
+++ src/kcm.go
@@ -9,7 +9,6 @@ import (
 
 var config_directory_path = fmt.Sprintf("%s/.kcm-test", get_user_home_direcotry())
 var full_config_directory_path = fmt.Sprintf("%s/.kcm-test/config.json", get_user_home_direcotry())
-var default_configuration = "{\"configs\": []}"
 
 func main() {
 	if !config_directory_present() {
@@ -17,20 +16,33 @@ func main() {
 	}
 
 	create_config_file_if_needed()
+	config := read_config_file()
 
 	switch command := os.Args[1]; command {
 	case "list":
+		list_configs(config)
 		fmt.Println("list")
 	case "status":
 		fmt.Println("status")
 	case "create":
+		name := os.Args[2]
+		path := os.Args[3]
+		active := "false"
+		configuration := add_config(config, name, path, active)
+		write_config(configuration)
 		fmt.Println("create")
 	case "remove":
+		//name := os.Args[2]
 		fmt.Println("remove")
 	case "activate":
+		//name := os.Args[2]
 		fmt.Println("activate")
 	default:
+		fmt.Println("KCM - Kubectl Configuration Manager")
+		fmt.Println(" ")
 		fmt.Println("Unknown command provided: ", command)
+		fmt.Println(" ")
+		fmt.Println("Help info should go here?")
 	}
 }
 
@@ -58,16 +70,14 @@ func check(e error) {
 
 func config_directory_present() bool {
 	if _, err := os.Stat(config_directory_path); err == nil {
-		fmt.Println("true")
 		return true
 	} else {
-		fmt.Println("false")
 		return false
 	}
 }
 
 func config_file_present() bool {
-	if _, err := os.Stat(full_config_directory_path); err != nil {
+	if _, err := os.Stat(full_config_directory_path); err == nil {
 		return true
 	} else {
 		return false
@@ -87,6 +97,40 @@ func create_config_file_if_needed() {
 		config_to_write, _ := json.MarshalIndent(default_config, "", " ")
 		fmt.Println(string(config_to_write))
 
-		ioutil.WriteFile(full_config_directory_path, config_to_write, 0755)
+		ioutil.WriteFile(full_config_directory_path, config_to_write, 0644)
+	}
+}
+
+func read_config_file() config_container {
+	var config_obj config_container
+
+	jsonFile, err := os.Open(full_config_directory_path)
+	check(err)
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &config_obj)
+
+	return config_obj
+}
+
+func write_config(configuration config_container) {
+	config_to_write, _ := json.MarshalIndent(configuration, "", " ")
+	ioutil.WriteFile(full_config_directory_path, config_to_write, 0644)
+}
+
+func add_config(configuration config_container, name string, path string, active string) config_container {
+	new_config := config{Name: name, Path: path, Active: active}
+	existing_configs := configuration.Configs
+	fmt.Println(existing_configs)
+	configuration.Configs = append(existing_configs, new_config)
+	fmt.Println(configuration)
+	return configuration
+}
+
+func list_configs(configuration config_container) {
+	for i := 0; i < len(configuration.Configs); i++ {
+		fmt.Println(configuration.Configs[i].Name + " - " + configuration.Configs[i].Path)
 	}
 }
